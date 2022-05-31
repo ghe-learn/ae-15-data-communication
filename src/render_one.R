a350200b73da1f6798a16ff92c3d0ea9cff84087
Lars Schöbitz <lars@lse.de>
2022-05-31 16:28:07 +0200

    Remove parameters exercise

--- src/render_one.R
+++
@@ -1,9 +0,0 @@
-render_one <- function(country) {
-  # assuming the output format of input.Rmd is PDF
-  quarto::quarto_render(
-    input = "ae-15b-parameters.qmd",
-    output_file = paste0("slides/", "waste-composition-", country, ".html"),
-    execute_params = list(country = country)
-    #envir = parent.frame()
-  )
-}
